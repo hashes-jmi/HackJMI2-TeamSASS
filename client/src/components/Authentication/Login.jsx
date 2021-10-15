ec8ab7152ead326f201b2a91cebd7d300b43c7ee
Mohd Saif Ansari <mhodsaif.sps@gmail.com>
2021-10-15 16:50:03 +0530

    new front end

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import classes from "./Login.module.css";
 import Card from "../UI/Card/Card";
 import Button from "../UI/Buttons/Button";
+import { useState } from "react";
+import axios from "axios";
 function Login() {
+  const [addhar,setaadhar]=useState('');
+
   return (
     <Card className={classes["login-modal"]}>
       <div className={classes.heading}>Login</div>
-      <input type="number" placeholder="Enter Adhar Number..." />
-      <Button title="Send OTP" />
+      <input type="number" placeholder="Enter Adhar Number..." onChange={(e)=>{setaadhar(e.target.value)}} value={addhar} />
+      <Button title="Send OTP" onClick={(e)=>{
+        axios.post()
+      }} />
       <p>Don’t have an account? Sign up here!</p>
     </Card>
   );
