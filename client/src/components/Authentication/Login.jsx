ee590c6c52b6d1f41d3cdf849fdb397c44425eda
Mohd Saif Ansari <mhodsaif.sps@gmail.com>
2021-10-15 23:52:24 +0530

    history error

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -8,8 +8,9 @@ import Input from "./../UI/Input/Input";
 import { useHistory } from "react-router";
 function Login() {
   const [addhar, setaadhar] = useState("");
+  const history=useHistory();
   const loginHandler = () => {
-    useHistory().push("/verify");
+    history.push("/verify");
   };
 
   return (
